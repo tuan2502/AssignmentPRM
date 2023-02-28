ca7c432c9b47fe775766390d2524851cc1ff578b
hieunmtse150772 <nguyenminhtrunghieu10@gmail.com>
2023-03-01 00:53:12 +0700

    Add slider image product detail

--- app/src/main/java/com/example/productshop/views/MainActivity.java
+++ app/src/main/java/com/example/productshop/views/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.productshop.R;
 
 public class MainActivity extends AppCompatActivity {
 
     NavController navController;
+    private Button button;
 
 
     @Override
@@ -21,5 +25,16 @@ public class MainActivity extends AppCompatActivity {
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProductDetail();
+            }
+        });
+    }
+    private void openProductDetail() {
+        Intent intent = new Intent(this, ProductDetailActivity.class);
+        startActivity(intent);
     }
 }
